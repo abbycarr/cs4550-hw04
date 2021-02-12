7b467c06ae7de583894dd113c7f4452f1621d4ab
abbycarr <carr.ab@husky.neu.edu>
2021-02-12 14:26:17 -0500

    logic work, todos

--- lib/practice/calc.ex
+++ lib/practice/calc.ex
@@ -1,25 +1,102 @@
 defmodule Practice.Calc do
-  def eval(list) do
-    cond do 
-      list.length == 1 ->
-        list |> hd
-      # list.lenght > 1 ->
-      end
+
+  def is_op(c) do
+    cond do
+      c=="+"||c=="-"||c=="*"||c=="/" ->
+        true
+      true -> 
+        false
     end
+  end
+
+  def importance(c) do
+    cond do
+      c=="*" || c=="/" ->
+        2
+      c=="+" || c=="-" ->
+        1
+      true ->
+        -1
+    end
+  end
+
+  def operate(c, n1, n2) when is_float(n1) do
+    cond do
+      c=="+" ->
+        n1 + String.to_integer(n2)
+      c=="-" ->
+        n1 - String.to_integer(n2)
+      c=="*" ->
+        n1 * String.to_integer(n2)
+      true ->
+        n1 / String.to_integer(n2)
+    end
+  end
+
+  def operate(c, n1, n2) when is_integer(n1) do
+    cond do
+      c=="+" ->
+        n1 + String.to_integer(n2)
+      c=="-" ->
+        n1 - String.to_integer(n2)
+      c=="*" ->
+        n1 * String.to_integer(n2)
+      true ->
+        n1 / String.to_integer(n2)
+    end
+  end
+
+  def operate(op, n1, n2), do: operate(op, String.to_integer(n1), n2)
+
+  def add_op(op, list) do
+    cond do 
+      length(list) == 0 ->
+        [op]
+      importance(op) > importance(hd(list)) ->
+        [op] ++ list
+      true ->
+        list ++ [op]
+    end
+  end
+
+  def ends(ex) do
+    length(ex) - 1
+  end
+
+  def to_post(expr, out, ops) do
+    cond do
+      length(expr) == 0 ->
+        eval(0, out, ops)
+
+      is_op(hd(expr)) ->
+        to_post(Enum.slice(expr,1..(ends(expr))), out, add_op(hd(expr), ops))
+      
+      length(out) > 0 ->
+        to_post(Enum.slice(expr,1..(ends(expr))), out ++ [hd(expr)],ops)
+      
+      true ->
+        to_post(Enum.slice(expr,1..(ends(expr))),[hd(expr)],ops)
+    end
+  end
+
+  def eval(total, out, ops) do
+    cond do
+      length(out) == 0 ->
+        total
+      length(out) == 1 && length(ops) == 0 ->
+        String.to_integer(hd(out))
+      total == 0 ->
+        eval(operate(hd(ops),hd(out),Enum.at(out,1)),Enum.slice(out,2..(ends(out))),Enum.slice(ops,1..(ends(ops))))
+      true ->
+        eval(operate(Enum.at(ops, 0),total,Enum.at(out,0)),Enum.slice(out,1..(ends(out))),Enum.slice(ops,1..(ends(ops))))
+    end
+  end
+
+  def splits(ex) do
+    String.split(ex, ~r/\s+/)
+  end
 
   def calc(expr) do
-    # This should handle +,-,*,/ with order of operations,
-    # but doesn't need to handle parens.
-
-    expr
-    |> String.split(~r/\s+/)
-
-    # Hint:
-    # expr
-    # |> split
-    # |> tag_tokens  (e.g. [+, 1] => [{:op, "+"}, {:num, 1.0}]
-    # |> convert to postfix
-    # |> reverse to prefix
-    # |> evaluate as a stack calculator using pattern matching
+    to_post(splits(expr),[], [])
   end
 end
--- lib/practice/factor.ex
+++ lib/practice/factor.ex
@@ -1,7 +1,23 @@
 defmodule Practice.Factor do
   
+  defp is_prime(x) do
+    2..x 
+    |> Enum.filter(fn a -> rem(x, a) == 0 end) 
+    |> length() == 1 
+  end
+  
+  
+  def next_prime(n) do
+    cond do
+      is_prime(n) ->
+        n
+      true ->
+        next_prime(n + 1)
+      end
+  end
+
   def factor(x) when is_integer(x), do: factor(x, 2, []) |> Enum.sort()
-  def factor(x), do: factor(Integer.parse(x), 2, []) |> Enum.sort()
+  def factor(x), do: factor(String.to_integer(x), 2, []) |> Enum.sort()
   
   def factor(x, current, factors) when x < current, do: factors
   def factor(x, current, factors) when x == current, do: [current | factors]
