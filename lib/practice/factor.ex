25bda7622d80207cb2c508744c279f5c3e338f86
abbycarr <carr.ab@husky.neu.edu>
2021-02-11 21:32:57 -0500

    Factor functionality fixed, status filled out preemptively

---
+++ lib/practice/factor.ex
@@ -0,0 +1,10 @@
+defmodule Practice.Factor do
+  
+  def factor(x), do: factor(x, 2, []) |> Enum.sort()
+
+  def factor(x, current, factors) when x < current, do: factors
+  def factor(x, current, factors) when x == current, do: [current | factors]
+  def factor(x, current, factors) when rem(x, current)==0, do: factor(div(x,current), 2, [current | factors])
+  def factor(x, current, factors), do: factor(x, current + 1, factors)
+
+end
